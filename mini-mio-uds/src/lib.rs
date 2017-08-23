3fdb79ac95354bde2dccaac37dc2e19c449a45e4
Dan Glastonbury <dan.glastonbury@gmail.com>
2017-08-23 11:14:29 +1000

    mini-mio-uds: Import mio-uds.
    To work around vendoring issues in gecko, import mio-uds code and trim
    down the files to the pieces that are required.

---
+++ mini-mio-uds/src/lib.rs
@@ -0,0 +1,25 @@
+//! MIO bindings for Unix Domain Sockets
+
+#![cfg(unix)]
+#![deny(missing_docs)]
+#![doc(html_root_url = "https://docs.rs/mio-uds/0.6")]
+
+extern crate libc;
+extern crate mio;
+
+use std::io;
+
+mod listener;
+mod socket;
+mod stream;
+
+pub use stream::UnixStream;
+pub use listener::UnixListener;
+
+fn cvt(i: libc::c_int) -> io::Result<libc::c_int> {
+    if i == -1 {
+        Err(io::Error::last_os_error())
+    } else {
+        Ok(i)
+    }
+}
