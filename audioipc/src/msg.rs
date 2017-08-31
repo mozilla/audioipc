aea179a81f6bf937c7586dd5ebd16887b57c52eb
Dan Glastonbury <dan.glastonbury@gmail.com>
2017-08-31 15:11:31 +1000

    audioipc: Remove kludge to handle EAGAIN/EWOULDBLOCK.

--- audioipc/src/msg.rs
+++ audioipc/src/msg.rs
@@ -1,27 +1,15 @@
 use libc;
 use std::io;
 use std::mem;
-use std::ptr;
 use std::os::unix::io::RawFd;
-use std;
+use std::ptr;
 
 // Note: The following fields must be laid out together, the OS expects them
 // to be part of a single allocation.
 #[repr(C)]
 struct CmsgSpace {
     cmsghdr: libc::cmsghdr,
-    data: libc::c_int,
-}
-
-unsafe fn sendmsg_retry(fd: libc::c_int, msg: *const libc::msghdr, flags: libc::c_int) -> libc::ssize_t {
-    loop {
-        let r = libc::sendmsg(fd, msg, flags);
-        if r == -1 && io::Error::last_os_error().raw_os_error().unwrap() == libc::EAGAIN {
-            std::thread::yield_now();
-            continue;
-        }
-        return r;
-    }
+    data: libc::c_int
 }
 
 pub fn sendmsg(fd: RawFd, to_send: &[u8], fd_to_send: Option<RawFd>) -> io::Result<usize> {
@@ -50,22 +38,11 @@ pub fn sendmsg(fd: RawFd, to_send: &[u8], fd_to_send: Option<RawFd>) -> io::Resu
 
     cmsg.data = fd_to_send.unwrap_or(-1);
 
-    let result = unsafe { sendmsg_retry(fd, &msghdr, 0) };
+    let result = unsafe { libc::sendmsg(fd, &msghdr, 0) };
     if result >= 0 {
         Ok(result as usize)
     } else {
         Err(io::Error::last_os_error())
-    }
-}
-
-unsafe fn recvmsg_retry(fd: libc::c_int, msg: *mut libc::msghdr, flags: libc::c_int) -> libc::ssize_t {
-    loop {
-        let r = libc::recvmsg(fd, msg, flags);
-        if r == -1 && io::Error::last_os_error().raw_os_error().unwrap() == libc::EAGAIN {
-            std::thread::yield_now();
-            continue;
-        }
-        return r;
     }
 }
 
@@ -87,16 +64,17 @@ pub fn recvmsg(fd: RawFd, to_recv: &mut [u8]) -> io::Result<(usize, Option<RawFd
     };
     iovec.iov_len = to_recv.len();
 
-    let result = unsafe { recvmsg_retry(fd, &mut msghdr, 0) };
+    let result = unsafe { libc::recvmsg(fd, &mut msghdr, 0) };
     if result >= 0 {
         let fd = if msghdr.msg_controllen == mem::size_of::<CmsgSpace>() as _ &&
             cmsg.cmsghdr.cmsg_len == mem::size_of::<CmsgSpace>() as _ &&
             cmsg.cmsghdr.cmsg_level == libc::SOL_SOCKET &&
-            cmsg.cmsghdr.cmsg_type == libc::SCM_RIGHTS {
-                Some(cmsg.data)
-            } else {
-                None
-            };
+            cmsg.cmsghdr.cmsg_type == libc::SCM_RIGHTS
+        {
+            Some(cmsg.data)
+        } else {
+            None
+        };
 
         Ok((result as usize, fd))
     } else {
