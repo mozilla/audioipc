713573c02125798be6bd7766d317efec1fee9154
Andreas Pehrson <apehrson@mozilla.com>
2024-01-12 00:04:45 +0100

    Bump cubeb-rs to 0.12 and handle ABI changes

--- server/src/server.rs
+++ server/src/server.rs
@@ -13,6 +13,7 @@ use audioipc::messages::{
 };
 use audioipc::shm::SharedMem;
 use audioipc::{ipccore, rpccore, sys, PlatformHandle};
+use cubeb::InputProcessingParams;
 use cubeb_core as cubeb;
 use cubeb_core::ffi;
 use std::convert::{From, TryInto};
@@ -509,6 +510,11 @@ impl CubebServer {
                 .map(ClientMessage::ContextPreferredSampleRate)
                 .unwrap_or_else(error),
 
+            ServerMessage::ContextGetSupportedInputProcessingParams => context
+                .supported_input_processing_params()
+                .map(|params| ClientMessage::ContextSupportedInputProcessingParams(params.bits()))
+                .unwrap_or_else(error),
+
             ServerMessage::ContextGetDeviceEnumeration(device_type) => context
                 .enumerate_devices(cubeb::DeviceType::from_bits_truncate(device_type))
                 .map(|devices| {
@@ -584,6 +590,18 @@ impl CubebServer {
                 .current_device()
                 .map(|device| ClientMessage::StreamCurrentDevice(Device::from(device)))
                 .unwrap_or_else(error),
+
+            ServerMessage::StreamSetInputMute(stm_tok, mute) => try_stream!(self, stm_tok)
+                .set_input_mute(mute)
+                .map(|_| ClientMessage::StreamInputMuteSet)
+                .unwrap_or_else(error),
+
+            ServerMessage::StreamSetInputProcessingParams(stm_tok, params) => {
+                try_stream!(self, stm_tok)
+                    .set_input_processing_params(InputProcessingParams::from_bits_truncate(params))
+                    .map(|_| ClientMessage::StreamInputProcessingParamsSet)
+                    .unwrap_or_else(error)
+            }
 
             ServerMessage::StreamRegisterDeviceChangeCallback(stm_tok, enable) => {
                 try_stream!(self, stm_tok)
